097fab3149309b8ba032c294930d2364f3a465f9
Sumit112234 <sumitbaghel22a@gmail.com>
2024-08-25 10:56:05 +0530

    chages done

---
+++ db/databaseConnection.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose');
+// import { mongoose } from 'mongoose'; 
+// import 'dotenv';
+
+
+async function dbConnect(){
+       
+        if (mongoose.connection.readyState) {
+            console.log('Already connected to MongoDB');
+            return true;
+        }
+        else{    
+          
+             
+                mongoose.connect(process.env.MONGO_URI)
+                .then(() => {
+                    console.log('Connected to MongoDB');
+                    return true;
+                }).catch((err) => {
+                    console.error('MongoDB connection error:', err);
+                    return false;
+                });
+        }
+
+}
+module.exports = dbConnect; 
+// export default dbConnect;
